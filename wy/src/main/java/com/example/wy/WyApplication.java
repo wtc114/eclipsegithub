0a65e3928b1305a21a0c422a8b7b6a2a0220bcc3
wtc114 <wtc114@qq.com>
2023-05-25 14:55:03 +0800

    my first git push

---
+++ wy/src/main/java/com/example/wy/WyApplication.java
@@ -0,0 +1,14 @@
+package com.example.wy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyApplication.class, args);
+		//test
+	}
+
+}
